f353b2779fcb6462fe67fc0b17d3e05749521d56
Forum-123 <69541616+Forum-123@users.noreply.github.com>
2021-12-01 18:28:07 +0000

    Fix error with render shortened URL on homepage

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 import sqlite3
-from flask import Flask, jsonify, redirect, render_template, request
+from flask import Flask, jsonify, redirect, render_template, request, url_for
 from hashids import Hashids
 from werkzeug import exceptions
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = "our-secret-pass"
-
-hashids = Hashids(min_length=5, salt=app.config['SECRET_KEY'])
 
 def connect_to_db():
     connection = sqlite3.connect('shortener/database.db')
     connection.row_factory = sqlite3.Row
     return connection
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "our-secret-pass"
+
+hashids = Hashids(min_length=5, salt=app.config['SECRET_KEY'])
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -19,7 +19,11 @@ def home():
 
     if request.method == 'POST':
         url = request.form['url']
-        url_query = connection.execute('INSERT INTO URLs (original_url) VALUES (?)', (url))
+
+        if not url:
+            return redirect(url_for('home'))
+
+        url_query = connection.execute('INSERT INTO URLs (original_url) VALUES (?)', (url,))
 
         connection.commit()
         connection.close()
@@ -29,30 +33,33 @@ def home():
         shorten_url = request.host_url + hash_id
 
         return render_template('home.html', short_url=shorten_url)
-    else:
-        return render_template('home.html', title='Short.io!')
+    return render_template('home.html', title='Short.io!')
 
-@app.route('/<url_id>')
-def redirect(url_id):
-    """Increment clicks value whenever the shortened URL is visited"""
-    connection = connect_to_db()
+# @app.route('/<url_id>', methods=['GET'])
+# def redirect(url_id):
+#     """Increment clicks value whenever the shortened URL is visited"""
+#     connection = connect_to_db()
 
-    original_id = hashids.decode(id)
-    if original_id:
-        original_id = original_id[0]
-        url_query = connection.execute('SELECT original_url, clicks FROM URLs WHERE id = (?)', (original_id)).fetchone()
+#     original_id = hashids.decode(id)
+#     if original_id:
+#         original_id = original_id[0]
+#         print(original_id)
+#         url_query = connection.execute('SELECT original_url, clicks FROM URLs WHERE id = (?)', (original_id,)).fetchone()
 
-        original_url = url_query['original_url']
-        clicks = url_query['clicks']
+#         original_url = url_query['original_url']
+#         clicks = url_query['clicks']
 
-        connection.execute('UPDATE URLs SET clicks = ? WHERE id = ?',
-                     (clicks + 1, original_id))
+#         connection.execute('UPDATE URLs SET clicks = ? WHERE id = ?',
+#                      (clicks + 1, original_id))
 
-        connection.commit()
-        connection.close()
-        return redirect(original_url)
-    else:
-        return redirect('home.html')
+#         connection.commit()
+#         connection.close()
+#         print(url_id)
+#         print(original_id)
+#         print(original_url)
+#         return redirect(original_url)
+#     else:
+#         return redirect(url_for('home'))
 
 @app.route('/stats')
 def stats():
